4d5ba7781cd313c5b452c2c9c74c968d6564a19a
anjanjanj <anjanjanj@users.noreply.github.com>
2015-09-25 18:45:15 +0800

    initial commit

---
+++ client/app/main/main.js
@@ -0,0 +1,11 @@
+'use strict';
+
+angular.module('voteapp2App')
+  .config(function ($stateProvider) {
+    $stateProvider
+      .state('main', {
+        url: '/',
+        templateUrl: 'app/main/main.html',
+        controller: 'MainCtrl'
+      });
+  });
